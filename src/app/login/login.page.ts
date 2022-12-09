b033cdd44a468bb20d642901d3a334695af2360d
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2022-12-08 21:23:25 -0300

    v1.5

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    alert('Olá, bem-vindo ao FormsApp. Esse app foi desenvolvido para estudo e conta com alguns erros.');
+    alert('Peço que ignore os erros e aproveite o app. Outros projetos (Sem erros) podem ser encontrados em github.com/vitorlopesnunes21');
+  }
 
   ngOnInit() {
   }
+
 
 }
